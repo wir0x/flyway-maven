ec4569f89159fe97fdaa2c583026668a79fcf7c0
wir0x <gsalazar-v@outlook.com>
2016-07-29 14:22:39 -0400

    empty project

---
+++ src/main/java/ch/swissbytes/app/App.java
@@ -0,0 +1,13 @@
+package ch.swissbytes.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
